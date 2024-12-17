64f0a7002390c443ecab9a1510def58c35e1a58c
agionoja <pauldivineagionoja@gmail.com>
2024-12-17 21:22:09 +0100

    Chore: fixed typo in spin server

--- app/routes/resources/spin-server.ts
+++ app/routes/resources/spin-server.ts
@@ -1,4 +1,4 @@
-import type { Route } from "./+types/s";
+import type { Route } from "./+types/spin-server";
 
 export async function loader({ request }: Route.LoaderArgs) {
   return Response.json({ message: "Spinning server" });
